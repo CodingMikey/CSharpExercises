fa1610e05fda0da922f148d5ae44ba4f347b7f6f
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-06-30 11:11:05 -0700

    Printing all odd numbers of an arrary
    Method to print odd numbers of an array

---
+++ PrintOddNumbers/PrintOddNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Given an array of integers, write a method to print only the odd numbers.
+
+            int[] arr = new int[] { 4, 5, 7, 9, 3 };
+
+            int i = 0;
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                //condition to check ODD number
+                if (arr[i] % 2 != 0)
+                    Console.Write(arr[i] + " ");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
