0a11155ca5f0c1c27175a0be49f3bbdfdd5c974d
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-07-01 12:30:03 -0700

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FizzBuzz Challenge
+
+            for (int i = 1; i <= 31; i++)
+            {
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if (fizz && buzz)
+                    Console.WriteLine("FizzBuzz");
+                else if (fizz)
+                    Console.WriteLine("Fizz");
+                else if (buzz)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
