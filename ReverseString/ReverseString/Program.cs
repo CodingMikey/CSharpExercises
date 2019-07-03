580bda7d0c2856bed6168b145ffd72b5f6f4300d
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-07-02 18:50:43 -0700

    ReverseString
    Reverse String

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Challenge: Given a string, reverse it.
+
+            string name = "Tesla";
+            char[] rs = name.ToArray();
+            Array.Reverse(rs);
+            Console.WriteLine(rs);
+            Console.ReadLine();
+        }
+    }
+}
