2ab5cd0d19fd1cedf5fd5bb4692e748a777e308a
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-12-17 14:30:01 -0800

    Using for loop to repeat a count 5 times
    Using for loop to repeat a count 5 times

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Count from 1 to 10 then back to 1
+            //and repeat this loop five times
+
+            for (int x = 0; x <= 5; x++)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                for (int i = 10; i >= 1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
