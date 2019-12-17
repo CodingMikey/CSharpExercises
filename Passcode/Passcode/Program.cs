ebc810bd5326ac1832b7d3ffda0c094977065b96
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-12-17 14:15:51 -0800

    Password checker
    Using while loop and if statement to check a password

---
+++ Passcode/Passcode/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Passcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = "";
+
+            while (code != "secret")
+            {
+                Console.WriteLine("Enter the password:");
+                code = Console.ReadLine();
+
+                if (code != "secret")
+                {
+                    Console.WriteLine("Not Authenticated");
+                }
+            }
+            Console.WriteLine("Authenticated!");
+            Console.ReadLine();
+        }
+    }
+}
