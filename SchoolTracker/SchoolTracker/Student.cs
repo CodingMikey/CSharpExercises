af0364a8b29d4a5b7ffd4aea43d3a5deb312a966
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2019-12-22 13:53:30 -0800

    Refactoring with OOP
    Refactoring with OOP

---
+++ SchoolTracker/SchoolTracker/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolTracker
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public string Birthday { get; set; }
+        public string Address { get; set; }
+        public int Phone { get; set; }
+    }
+}
