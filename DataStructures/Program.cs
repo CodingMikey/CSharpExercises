3b99b446831a4ef80c47074618cafdd60ebd1d6a
Michael Galas <39977309+CodingMikey@users.noreply.github.com>
2020-11-30 09:20:01 -0800

    Add files via upload

---
+++ DataStructures/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList myList = new SinglyLinkedList();
+            myList.insertFirst(100);
+            myList.insertFirst(50);
+            myList.insertFirst(99);
+            myList.insertFirst(88);
+            myList.displayList();
+        }
+    }
+
+    public class SinglyLinkedList
+    {
+        private Node first;
+        public bool isEmpty()
+        {
+            return (first == null);
+        }
+
+        public void insertFirst(int data)
+        {
+            Node newNode = new Node
+            {
+                data = data,
+                next = first
+            };
+            first = newNode;
+        }
+
+        public Node deleteFirst()
+        {
+            Node temp = first;
+            first = first.next;
+            return temp;
+        }
+
+        public void displayList()
+        {
+            Console.WriteLine("List (first -- last) ");
+            Node current = first;
+            while (current != null)
+            {
+                current.displayNodeMethod();
+                current = current.next;
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        public void insertLast(int data)
+        {
+            Node current = first;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            Node newNode = new Node();
+            newNode.data = data;
+            current.next = newNode;
+        }
+    }
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public void displayNodeMethod()
+        {
+            Console.WriteLine("<" + data + ">");
+            
+        }
+        
+    }
+}
